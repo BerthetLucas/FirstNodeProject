27151ba62823c47e78a15d9e882b0899572b62bf
Lucas Berthet <lu.berthet@gmail.com>
2023-11-22 11:31:48 +0100

    display table on  webpage

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 // get the client
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+const express = require("express");
+const app = express();
+const port = 3000;
+
 
 // create the connection to database
 const connection = mysql.createConnection({
-  host: 'localhost',
-  port : '3306',
-  user: 'root',
-  password: '',
-  database: 'dwwm_20_10_2023',
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "",
+  database: "dwwm_20_10_2023",
 });
 
-// simple query
-connection.query(
-  'SELECT * FROM pictures',
-  function(err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
-  }
-);
+const displayResults = (request, reponse) => {
+  // simple query
+  connection.query("SELECT * FROM pictures", function (err, results, fields) {
+    reponse.send(results);
+  });
+};
+
+app.get("/", displayResults);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+
