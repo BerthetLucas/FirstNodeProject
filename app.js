c7a6e319a37f4c3c0fcf7c89ef698d8a511b6a8c
Lucas Berthet <lu.berthet@gmail.com>
2023-11-22 14:18:52 +0100

    add view to cut between the view and the api

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 const port = 3000;
 
 
+
+
 // create the connection to database
 const connection = mysql.createConnection({
   host: "localhost",
@@ -15,19 +17,22 @@ const connection = mysql.createConnection({
 });
 
 const displayResults = (request, reponse) => {
-const limit = request.query.limit; 
+
+const limit = !isNaN(request.query.limit) ? parseInt(request.query.limit) : 3;  
 
     // simple query
   connection.query("SELECT * FROM pictures ORDER BY id DESC LIMIT " + limit, 
   function (err, results, fields) {
     reponse.send(results);
+    console.log(err); 
   });
 };
 
-app.get("/", displayResults);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
 
+app.use('/',express.static("public")); 
+app.get("/api", displayResults);
