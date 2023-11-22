12b7106b0240e458c908682c217a6c93180a61ae
Lucas Berthet <lu.berthet@gmail.com>
2023-11-22 14:56:41 +0100

    add some more element from the homemade API

--- public/script.js
+++ public/script.js
@@ -14,11 +14,16 @@ function display(data) {
    const dataTest = data; 
 
     data.forEach((item) => {
-
+        title = document.createElement('h2'); 
+        title.innerText = item.title; 
         img = document.createElement('img'); 
         img.setAttribute("src", item.image); 
+        desc = document.createElement('p'); 
+        desc.innerText = item.description; 
 
+        container.appendChild(title); 
         container.appendChild(img); 
+        container.appendChild(desc); 
     }); 
 
 }
