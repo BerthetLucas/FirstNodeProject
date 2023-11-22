3042cdbe187d9c92ef29847cbf23e7bd295dad6e
Lucas Berthet <lu.berthet@gmail.com>
2023-11-22 16:55:10 +0100

    add insert function

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // get the client
 const mysql = require("mysql2");
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // create the connection to database
 const connection = mysql.createConnection({
@@ -18,7 +18,7 @@ const connection = mysql.createConnection({
 
 const displayResults = (request, reponse) => {
 
-const limit = !isNaN(request.query.limit) ? parseInt(request.query.limit) : 3;  
+const limit = !isNaN(request.query.limit) ? parseInt(request.query.limit) : 2;  
 
     // simple query
   connection.query("SELECT * FROM pictures ORDER BY id DESC LIMIT " + limit, 
@@ -36,3 +36,19 @@ app.listen(port, () => {
 
 app.use('/',express.static("public")); 
 app.get("/api", displayResults);
+
+
+app.post('/api',(request,reponse)=>{
+
+console.log(request.body.title); 
+console.log(request.body.image); 
+console.log(request.body.description); 
+
+connection.query(`INSERT INTO pictures (title,image,description) VALUES (?,?,?)`, [request.body.title, request.body.image, request.body.description],
+
+function (err, results, fields) {
+  reponse.send(results);
+  console.log(err); 
+});
+
+}); 
